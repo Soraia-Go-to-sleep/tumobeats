ed520245d368972afd2e6d64be1c8b03b88e3fc8
Soraia-Go-to-sleep <soraia.urbano@tumo.world>
2025-07-16 13:27:02 +0100

    aula 3

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-window.onload = function() {
-    
+window.onload = async function() {
 
+    //Carregar dados da internet (data.json)
+    let request = await fetch("data.json");
+    let audioData = await request.json();
+
+    
     // Variaveis de Elementos
     let title = document.querySelector("#title");
-    let previousButton = document.querySelector("#previous-button")
-    let playpauseButton = document.querySelector("#playpause-button")
-    let nextButton = document.querySelector("#next-button")
+    let previousButton = document.querySelector("#previous-button");
+    let playpauseButton = document.querySelector("#playpause-button");
+    let nextButton = document.querySelector("#next-button");
 
+    let scrubInput = document.querySelector("#scrub-input");
+    let volumeInput = document.querySelector("#volume-input");
+
+    let fileInput = document.querySelector("#file-input");
+
+    let audio = document.querySelector("audio");
+    let currentMusic = 0 
+    console.log(audioData[currentMusic]);
+  
 
 
 
@@ -15,17 +28,66 @@ window.onload = function() {
     function changeTitle(value) {
         title.innerText = value;
     }
+    function updateInputBar(value, bar) {
+        bar.style.transform = "scaleX(" + value / 100 + ")";
+    }
 
    previousButton.onclick = function() {
     console.log("previous button clicked");
    }
 
    playpauseButton.onclick = function() {
+
+    if(audio.paused) {
+        playAudio();
+    } else {
+        pauseAudio();
+    }
+
+
     console.log("playpause button clicked");
    }
 
    nextButton.onclick = function() {
     console.log("next button clicked");
+   }
+
+   scrubInput.querySelector("input").oninput = function(event) {
+    let bar = scrubInput.querySelector(".range-bar");
+
+    updateInputBar(event.target.value, bar);
+   }
+
+   volumeInput.querySelector("input").oninput = function(event) {
+    let bar = volumeInput.querySelector(".range-bar");
+
+    updateInputBar(event.target.value, bar);
+   }
+
+   fileInput.oninput = function() {
+    console.log("AQUI!")
+   }
+
+   function playAudio() {
+
+    audio.src = audioData[currentMusic].url;
+    audio.play()
+   }
+   function pauseAudio() {
+    audio.pause();
+   }
+
+   audio.onplay = function() {
+    let playIcon = document.querySelector("#icon-play");
+    let pauseIcon = document.querySelector("#icon-pause");
+    playIcon.style.display = "none";
+    pauseIcon.style.display = "block";
+   }
+   audio.onpause = function() {
+    let playIcon = document.querySelector("#icon-play");
+    let pauseIcon = document.querySelector("#icon-pause");
+    playIcon.style.display = "block";
+    pauseIcon.style.display = "none";
    }
 
     changeTitle("Soraia")
